ec71378f4b2f9e48153995b9938a7faa34b00d86
Kevin <admin@stardothosting.com>
2019-03-08 19:07:16 -0500

    moved everything into proper constructor and render functions , preliminary auto update textbox for testing results of scn

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, AppRegistry, ScrollView} from 'react-native';
+import {Platform, StyleSheet, Text, View, AppRegistry, ScrollView, FlatList} from 'react-native';
 
 
 // Custom requires
@@ -36,115 +36,6 @@ var ipRange = null;
 //var portRange = [ 20, 21, 22, 25, 80, 110, 139, 143, 443, 3389 ]
 var portRange = [ 80, 443 ]
 
-// Must load all variables in sequence asynchronously
-async.series([
-  /****************************
-  * Assign all local IP data  *
-  *****************************/
-  function(callback) {
-    NetworkInfo.getIPAddress(ip => { 
-      local_ip = ip; 
-      NetworkInfo.getBroadcast(address => {
-        local_broadcast = address; 
-        SubnetmaskModule.getSubnet((sb) => { 
-          local_netmask = sb; 
-          callback(); 
-        });
-      });
-    });
-  },
-  /*************************************************
-  * Assign All IP data once everything is obtained *
-  *************************************************/
-  function(callback) {
-      subconv = ipaddr.IPv4.parse(local_netmask).prefixLengthFromSubnetMask();
-      firstHost = ipaddr.IPv4.networkAddressFromCIDR(local_ip + "/" + subconv);
-      lastHost = ipaddr.IPv4.broadcastAddressFromCIDR(local_ip + "/" + subconv);
-      firstHostHex = sip.convertIPtoHex(firstHost);
-      lastHostHex = sip.convertIPtoHex(lastHost);
-      ipRange = sip.getIPRange(firstHostHex,lastHostHex);
-      ipRange = ipRange.slice(1); // Remove the first ip in the array
-      callback();
-  },
-  /*************************
-  * Start array loop scans *
-  *************************/
-  function(callback) {
-    // Loop through all IPs in a scan
-    async.eachSeries(ipRange, function(singleIP, callback) {
-      // Loop within the loop to cycle through array of common ports
-      async.eachSeries(portRange, function(singlePort, callbackPort) {
-        var q = async.queue(async.asyncify(async function(singlePort) {
-          return await scanTCPHost(singleIP, singlePort);
-        }));
-        console.log('************* scanning ' + singleIP); 
-        q.push(singlePort);  
-        callbackPort();
-      }, function(err) {
-        if (err) {
-          //console.log('error happened with port');
-        } else {
-          //console.log('success port!');
-        }
-      }
-      );
-        callback();
-      }, function(err) {
-        if (err) {
-          console.log('error happened');
-        } else {
-          console.log('success ip!');
-        }
-      }
-    );
-  }
-  ]);
-
-/*console.log('hey');
-var iphex = sip.convertIPtoHex('192.168.1.1');
-console.log('ip address : ' + iphex);
-
-console.log('net : ' + JSON.stringify(net));
-
-var client = net.createConnection(80, '10.0.1.102');
-console.log('Socket created.');
-client.on('data', function(data) {
-  // Log the response from the HTTP server.
-  console.log('RESPONSE: ' + data);
-}).on('connect', function() {
-  // Manually write an HTTP request.
-  client.write("GET / HTTP/1.0\r\n\r\n");
-}).on('end', function() {
-  console.log('DONE');
-});*/
-
-function scanTCPHost(host,port) {
-  var client = net.connect({
-    host: host,
-    port: port
-  },
-  function() { //'connect' listener
-    alert('connected to server : ' + host + ' on port : ' + port);
-    client.end();
-    return true;
-    //client.write('GET / HTTP/1.0\r\n\r\n');
-  });
-  client.on('error', function(err) {
-    //alert('error : ' + JSON.stringify(err));
-    client.end();
-    return false;
-  });
-  /*client.on('data', function(data) {
-    alert(JSON.stringify(data));
-    client.end();
-  });
-  client.on('end', function() {
-    console.log('disconnected from server');
-  });*/
-  return true;
-}
-
-
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -154,12 +45,107 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor() {
+    super();
+    this.state = {
+      listContent: 'hit me.'
+    }
+  }
+// Function to scan TCP Hosts
+scanTCPHost = (host,port) => {
+  var client = net.connect({
+    host: host,
+    port: port
+  },
+  function() { //'connect' listener
+    //alert('connected to server : ' + host + ' on port : ' + port);
+    client.end();
+    return true;
+    //client.write('GET / HTTP/1.0\r\n\r\n');
+  });
+  client.on('error', function(err) {
+    //alert('error : ' + JSON.stringify(err));
+    client.end();
+    return false;
+  });
+  return true;
+}
+  triggerScan = () => {
+    // Must load all variables in sequence asynchronously
+    async.series([
+      /****************************
+      * Assign all local IP data  *
+      *****************************/
+      function(callback) {
+        NetworkInfo.getIPAddress(ip => { 
+          local_ip = ip; 
+          NetworkInfo.getBroadcast(address => {
+            local_broadcast = address; 
+            SubnetmaskModule.getSubnet((sb) => { 
+              local_netmask = sb; 
+              callback(); 
+            });
+          });
+        });
+      },
+      /*************************************************
+      * Assign All IP data once everything is obtained *
+      *************************************************/
+      function(callback) {
+          subconv = ipaddr.IPv4.parse(local_netmask).prefixLengthFromSubnetMask();
+          firstHost = ipaddr.IPv4.networkAddressFromCIDR(local_ip + "/" + subconv);
+          lastHost = ipaddr.IPv4.broadcastAddressFromCIDR(local_ip + "/" + subconv);
+          firstHostHex = sip.convertIPtoHex(firstHost);
+          lastHostHex = sip.convertIPtoHex(lastHost);
+          ipRange = sip.getIPRange(firstHostHex,lastHostHex);
+          ipRange = ipRange.slice(1); // Remove the first ip in the array
+          callback();
+      },
+      /*************************
+      * Start array loop scans *
+      *************************/
+      function(callback) {
+        // Loop through all IPs in a scan
+        async.eachSeries(ipRange, function(singleIP, callback) {
+          // Loop within the loop to cycle through array of common ports
+          async.eachSeries(portRange, function(singlePort, callbackPort) {
+            var q = async.queue(async.asyncify(async function(singlePort) {
+              return await this.scanTCPHost(singleIP, singlePort);
+            }));
+            console.log('************* scanning ' + singleIP); 
+            q.push(singlePort);  
+            callbackPort();
+          }, function(err) {
+            if (err) {
+              //console.log('error happened with port');
+            } else {
+              //console.log('success port!');
+            }
+          }
+          );
+            callback();
+          }, function(err) {
+            if (err) {
+              console.log('error happened');
+            } else {
+              console.log('success ip!');
+            }
+          }
+        );
+      }
+      ]);
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.container}>
+        <Text onPress = {this.triggerScan}>
+          {this.state.listContent}
+        </Text>
+      </View>
       </View>
     );
   }
