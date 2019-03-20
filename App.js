35d830cc89f6181367ca38b6932a7affed14df7e
Kevin <admin@stardothosting.com>
2019-03-20 07:49:21 -0400

    latest changes and cleanup efforts

--- App.js
+++ App.js
@@ -8,8 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, AppRegistry, ScrollView, FlatList} from 'react-native';
-
+import {Platform, StyleSheet, Text, TextInput, View, AppRegistry, ScrollView, FlatList, Button} from 'react-native';
 
 // Custom requires
 import { NetworkInfo } from 'react-native-network-info';
@@ -34,7 +33,10 @@ var lastHostHex = null;
 var ipRange = null;
 var ipRange = null;
 //var portRange = [ 20, 21, 22, 25, 80, 110, 139, 143, 443, 3389 ]
-var portRange = [ 80, 443 ]
+var portRange = [ 80, 443 ];
+var scanResult = new Array();
+var res = null;
+var test_outside = null;
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -42,35 +44,20 @@ const instructions = Platform.select({
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-
+  
 type Props = {};
 export default class App extends Component<Props> {
-  constructor() {
-    super();
+
+  constructor(props) {
+    super(props);
     this.state = {
-      listContent: 'hit me.'
+      listContent: [],
     }
   }
-// Function to scan TCP Hosts
-scanTCPHost = (host,port) => {
-  var client = net.connect({
-    host: host,
-    port: port
-  },
-  function() { //'connect' listener
-    //alert('connected to server : ' + host + ' on port : ' + port);
-    client.end();
-    return true;
-    //client.write('GET / HTTP/1.0\r\n\r\n');
-  });
-  client.on('error', function(err) {
-    //alert('error : ' + JSON.stringify(err));
-    client.end();
-    return false;
-  });
-  return true;
-}
+
   triggerScan = () => {
+    var promise1 = new Promise(function(resolve, reject) {
+
     // Must load all variables in sequence asynchronously
     async.series([
       /****************************
@@ -105,47 +92,86 @@ scanTCPHost = (host,port) => {
       * Start array loop scans *
       *************************/
       function(callback) {
+
+        test_outside = '1234';
+
+
         // Loop through all IPs in a scan
         async.eachSeries(ipRange, function(singleIP, callback) {
           // Loop within the loop to cycle through array of common ports
-          async.eachSeries(portRange, function(singlePort, callbackPort) {
-            var q = async.queue(async.asyncify(async function(singlePort) {
-              return await this.scanTCPHost(singleIP, singlePort);
-            }));
-            console.log('************* scanning ' + singleIP); 
-            q.push(singlePort);  
-            callbackPort();
-          }, function(err) {
+          async.eachSeries(portRange, function(singlePort, callback) {
+            //var q = async.queue(
+              //async.asyncify(async function(callback) {
+                var client = net.connect({
+                  host: singleIP,
+                  port: singlePort
+                },
+                function(callback) { //'connect' listener
+                  //alert('connected to server : ' + singleIP + ' on port : ' + singlePort);
+
+                  scanResult.push('connected to server : ' + singleIP + ' on port : ' + singlePort);
+                  //alert(res);
+                  //scanResult.push(res);
+                  //client.end();
+                  //client.write('GET / HTTP/1.0\r\n\r\n');
+                });
+                client.on('error', function(err) {
+                  //alert('error : ' + JSON.stringify(err));
+                  client.end();
+                });
+            //}
+            //));
+            console.log('************* scanning ' + singleIP);
+
+            //alert(JSON.stringify(scanResult));
+            //q.push(singlePort);  
+            callback();
+          }, function(err, res) {
             if (err) {
               //console.log('error happened with port');
             } else {
               //console.log('success port!');
+              //alert(JSON.stringify(res));
+              alert(JSON.stringify(test_outside));
+
+
             }
           }
           );
             callback();
-          }, function(err) {
+          }, function(err,result) {
             if (err) {
               console.log('error happened');
             } else {
-              console.log('success ip!');
+              //alert(JSON.stringify(scanResult));
             }
           }
         );
+        alert(res);
+        callback(res);
+      },
+      function() {
+        console.log('last function');
       }
       ]);
+    }); // end promise
+    promise1.then(function(value) {
+      //alert(JSON.stringify(test_outside));
+      //console.log(JSON.stringify(value);
+
+      alert('****** ALL DONE***');
+      // expected output: "Success!"
+    });
+
   }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to Shift8 Scan!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-        <View style={styles.container}>
-        <Text onPress = {this.triggerScan}>
-          {this.state.listContent}
-        </Text>
-      </View>
+        <Button onPress = {this.triggerScan} title="hit me." color="#841584" accessibilityLabel="hit me."/>
+        <TextInput editable={false} ref={component=> this._MyComponent=component}/>
       </View>
     );
   }
@@ -161,11 +187,11 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    marginTop: -80,
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 0,
   },
 });
