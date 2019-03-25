443d71c199de256421743a79fb6b80e45b69cc77
Kevin <admin@stardothosting.com>
2019-03-25 15:50:35 -0400

    properly handling connections in scan , properly timing out , proper promises and displaying results as a list item

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ import { List, ListItem } from 'react-native-elements';
 // Custom requires
 import { NetworkInfo } from 'react-native-network-info';
 import SubnetmaskModule from 'get-subnet-mask';
+import TouchableScale from 'react-native-touchable-scale'; // https://github.com/kohver/react-native-touchable-scale
+import LinearGradient from 'react-native-linear-gradient'; // Only if no expo
 var sip = require ('shift8-ip-func');
 var net = require('react-native-tcp');
 var async = require("async");
@@ -31,30 +33,88 @@ var lastHostHex = null;
 var ipRange = null;
 var ipRange = null;
 var portRange = [ 80, 443 ];
-var scanResult = null;
+var scanResult = [];
 var scan = [];
 var test_var = null;
+
+// Function to add delay
+function sleeper(ms) {
+  return function(x) {
+    return new Promise(resolve => setTimeout(() => resolve(x), ms));
+  };
+}
+
+var fetchData = function () {
+ return new Promise(function (resolve, reject) {
+    resolve();
+  });
+};
 
 // Function to scan hosts
 var scanHost = function(hostIP, hostPort) {
   return new Promise(function (resolve,reject) {
+    //console.log('%%%%%%%%% DOING ' + hostIP + ' ON ' + hostPort)
     var client = net.connect({
       host: hostIP,
       port: hostPort
     }, 
     function() { //'connect' listener
-      client.end();
-      //console.log('@@@@@@@@@@@@@@@@@@@ CONNECTED ' + hostIP);
-      var scan_result = {
+      client.destroy();
+      console.log('Connected');
+    });
+    
+    client.setTimeout(2000,function(){
+        // called after timeout -> same as socket.on('timeout')
+        // it just tells that soket timed out => its ur job to end or destroy the socket.
+        // socket.end() vs socket.destroy() => end allows us to send final data and allows some i/o activity to finish before destroying the socket
+        // whereas destroy kills the socket immediately irrespective of whether any i/o operation is goin on or not...force destry takes place
+        console.log('Socket timed out');
+    });
+
+    client.on('connect', function() {
+        //sleeper(200);
+        //client.destroy();
+        var scan_result = {
         ip:hostIP, 
         port:hostPort
-      };
-      resolve(scan_result);
+        };
+        resolve(scan_result);
+    })
+    
+    client.on('timeout',function(){
+      console.log('Socket timed out !');
+      client.end('Timed out!');
+      // can call socket.destroy() here too.
     });
+
+    client.on('end',function(data){
+      console.log('Socket ended from other end!');
+      console.log('End data : ' + data);
+    });
+
+    client.on('close',function(error){
+      var bread = client.bytesRead;
+      var bwrite = client.bytesWritten;
+      //console.log('Bytes read : ' + bread);
+      //console.log('Bytes written : ' + bwrite);
+      //console.log('Socket closed!');
+      //if(error){
+      //  console.log('Socket was closed coz of transmission error');
+      //}
+    }); 
+
     client.on('error', function(err) {
+      sleeper(1000);
       console.log('******* ERROR : ' + JSON.stringify(err));
-      client.end();
+      client.destroy();
+      //reject(err);
     });
+
+    setTimeout(function(){
+      var isdestroyed = client.destroyed;
+      console.log('Socket destroyed:' + isdestroyed);
+      client.destroy();
+    },5000);
   });
 }
 
@@ -79,7 +139,6 @@ export default class App extends Component<Props> {
 
 
   triggerScan = () => {
-
     // Gather Network Info
     var network_promise = new Promise(function(resolve, reject) {
         NetworkInfo.getIPAddress(ip => { 
@@ -115,15 +174,46 @@ export default class App extends Component<Props> {
     network_promise.then((response) => {
       for (let i = 0; i < response["ip_range"].length; i++) {
         for (let j = 0; j < portRange.length; j++) {
-          scanHost(response["ip_range"][i], portRange[j]).then(response => {
-            this.setState(prevState => ({
-              listContent : [prevState.listContent, response]
-            }));
-          });
+          scanHost(response["ip_range"][i], portRange[j]) 
+          //.then(sleeper(5000))
+          .then(response => {
+            scanResult.push(response);
+            this.setState({ 
+              listContent: this.state.listContent.concat([response])
+            });
+          })
+          .catch(err => {
+            console.error(err);
+            return err;
+          })
         }
       }
-    });
+    })
+    .catch(err => {
+      console.error(err);
+      return err;
+    })
   }
+
+renderRow ({ item }) {
+  return (
+    (!item.length?
+      <ListItem
+        roundAvatar
+        style={{width:200, height:100}}
+        title={item.ip}
+        titleStyle={{ color: 'black', fontWeight: 'bold' }}
+        subtitleStyle={{ color: 'black' }}
+        subtitle={`Port : ${item.port}`}
+        chevronColor="black"
+        chevron
+      />
+      : null)
+  )
+}
+
+keyExtractor = (item, index) => index.toString()
+
   render() {
     return (
       <View style={styles.container}>
@@ -132,9 +222,10 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>{instructions}</Text>
         <Button onPress = {this.triggerScan} title="hit me." color="#841584" accessibilityLabel="hit me."/>
         <FlatList 
+           keyExtractor={this.keyExtractor}
            data={this.state.listContent}
            extraData={this.state.listContent}
-           renderItem={({item}) => <Text>{item.ip} {item.port}</Text>}
+           renderItem={this.renderRow}
         />
       </View>
     );
